9632467d35971ad693c412beda05ac2d35791aec
Cristhoper <cristhoper@gmail.com>
2019-05-28 00:34:08 -0400

    Implement microservice for a pokemon frontend

---
+++ uapp.py
@@ -0,0 +1,68 @@
+import json
+from threading import Lock
+import requests
+from flask import Flask
+from flask_cors import CORS
+
+# Lock to populate queue
+populate_lock = Lock()
+populate_lock.acquire()
+
+# Queue implemented, simple array.
+queue = []
+
+# Implement Flask with CORS, to allow Cross-Origin
+uapp = Flask(__name__)
+cors = CORS(uapp)
+
+# Get values for pokemon list.
+pokedata = requests.get("https://pokeapi.co/api/v2/pokemon/?limit=1", timeout=10)
+data = pokedata.json()
+# get max resource available
+max_element = int(data['count'])
+
+# Run GET using amx limit
+url = "https://pokeapi.co/api/v2/pokemon/?limit={}".format(max_element)
+pokedata = requests.get(url, timeout=10)
+data = pokedata.json()
+
+# populate queue
+for pokemon in data['results']:
+    p_id = str(pokemon['url']).split('/')[6]
+    queue.append({'id': p_id, 'name': pokemon['name']})
+offset = 950
+
+# Unlock queue, allowing connections.
+populate_lock.release()
+
+
+@uapp.route('/pokeapi/')
+def pokeapi():
+    """
+    Endpoint to get 18 elements, moving inside queue one by one.
+    """
+    # Check if populate is locked
+    if populate_lock.locked():
+        return
+
+    global offset, max_element
+
+    # Populate small array, to return just 18 elements.
+    poke_array = []
+    for i in range(18):
+        # Boundary condition. Run circular.
+        current_index = (i + offset) % max_element
+        poke_array.append({
+            'id': queue[current_index]['id'],
+            'name': queue[current_index]['name']
+        })
+    # Set boundary condition, to avoid endless increment.
+    offset = offset + 1 if offset + 1 < max_element else 0
+
+    # return JSON object service.
+    ret = json.dumps(poke_array)
+    return ret
+
+
+if __name__ == '__main__':
+    uapp.run(debug=True, port=5081)
